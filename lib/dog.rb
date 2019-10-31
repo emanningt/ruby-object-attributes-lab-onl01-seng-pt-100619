0b06070e042d176cec8c327bcfedfdb7ee29de2c
Eric Manning <emanningmusic@yahoo.com>
2019-10-31 19:22:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,10 @@
 class Dog 
-  def initialize(name)
-    @name = name 
-  end
-  
   def name=(dogs_name)
-    @name
+    @this_dogs_name = dogs_name
   end
   
+  def name 
+    @this_dogs_name
+  end
   
 end
